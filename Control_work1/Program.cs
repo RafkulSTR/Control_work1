cd275f34469bcf8103ef58e34c8c2735f2c2ae55
Rafik Kuliev <rombaks2019@gmail.com>
2023-08-03 05:36:38 +0300

    Добавьте файлы проекта.

---
+++ Control_work1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Control_work1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите элементы массива через пробел: ");
+            string input = Console.ReadLine();
+
+            string[] array = input.Split(' ');
+
+            
+        }
+
+        static string[] FilterArray(string[] array)
+        {
+           
+
+            return null;
+        }
+    }
+}
