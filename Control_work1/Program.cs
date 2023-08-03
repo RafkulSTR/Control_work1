5b1bc5c4922ea63700010fa597c4ea5996090995
Rafik Kuliev <rombaks2019@gmail.com>
2023-08-03 05:43:06 +0300

    Написан метод формирующий новый массив с элементами меньше ила равное 3

--- Control_work1/Program.cs
+++ Control_work1/Program.cs
@@ -9,15 +9,42 @@
 
             string[] array = input.Split(' ');
 
-            Console.WriteLine(string.Join(",", array));
+            Console.WriteLine("Новый массив:");
+            Console.WriteLine(string.Join(", ", FilterArray(array)));
 
         }
 
+        /// <summary>
+        /// Формирование нового массива, менее трех символов
+        /// </summary>
+        /// <param name="array">Массив символов введеных пользователем</param>
+        /// <returns>Готовый массив</returns>
         static string[] FilterArray(string[] array)
         {
-           
+            int newSize = 0;
 
-            return null;
+            foreach (string element in array)
+            {
+                if (element.Length <= 3)
+                {
+                    newSize++;
+                }
+            }
+
+            string[] filteredArray = new string[newSize];
+
+            int index = 0;
+
+            foreach (string element in array)
+            {
+                if (element.Length <= 3)
+                {
+                    filteredArray[index] = element;
+                    index++;
+                }
+            }
+
+            return filteredArray;
         }
     }
 }
